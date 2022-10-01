d7ab4c0452904942627b12ddeee44a2fe6795063
thureintun234 <thurein12456@gmail.com>
2022-10-01 07:03:02 +0630

    backend folder/project structure added

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+const config = require("./utils/config");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const app = express();
+
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to mongodb");
+  })
+  .catch((err) => {
+    logger.error("error connecting to mongodb", err.message);
+  });
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan(middleware.morganOptions));
+app.use(helmet());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+// validation middlewares
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
